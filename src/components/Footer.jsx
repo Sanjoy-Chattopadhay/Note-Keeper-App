64c9d492f393b2dd2e311ebccc5f040342386f6e
Sanjoy Chattopadhyay <chatterjeesanjoy347@gmail.com>
2022-08-18 00:49:25 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer>
-      <p>Copyright ⓒ {year}</p>
+      <p>Copyright ⓒ Sanjoy | {year}</p>
     </footer>
   );
 }
